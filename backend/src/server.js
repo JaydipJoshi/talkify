c2e53b9fdac9ff6f262bc5a2a0838b3cffdc143d
JaydipJoshi <jaydipjoshi43@gmail.com>
2025-09-30 22:55:57 +0530

    added arcject protection for bot detection

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,15 +6,14 @@ import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
 
-
-
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
+// payload too large error
 app.use(express.json()); // req.body
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
