2a10d4a1b93b930617be3976508b33df1eed9af8
JaydipJoshi <jaydipjoshi43@gmail.com>
2026-02-17 19:49:16 +0530

    JWT Update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true, //prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks: cross-site request forgery
-    secure: ENV.NODE_ENV === "development" ? false : true, // only send cookie over HTTPS in production
+    sameSite: isProduction ? "none" : "lax", // IMPORTANT: none in prod
+    secure: isProduction, // true in prod, false locally
   });
   return token;
 };
